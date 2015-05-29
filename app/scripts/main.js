fd98a55f4314abdf1d953c74113dfe7534c35d47
Scott Warner <scottbwarner@gmail.com>
2015-05-29 18:26:47 -0400

    Initial commit. Yeoman-generated boilerplate

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global HomeFe, $*/
+
+
+window.HomeFe = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    HomeFe.init();
+});
